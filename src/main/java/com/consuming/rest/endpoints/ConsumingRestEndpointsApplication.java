c52492d57945a09b5a8d680c3d8b8067b3791e88
thanhnd <ducthanhnguyen95@gmail.com>
2023-05-31 23:04:34 +0700

    Consuming rest endpoints

---
+++ src/main/java/com/consuming/rest/endpoints/ConsumingRestEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.consuming.rest.endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestEndpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consuming/rest/endpoints/ConsumingRestEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consuming.rest.endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
